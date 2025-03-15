0f7eb1c66a2d84ce58b957ec42d38d1eb1842d9f
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-03-15 08:13:48 +0530

    done

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ const User = require('./schema');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 4141;
+const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(bodyParser.json());
 
-// MongoDB connection
 mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to database'))
   .catch(err => console.error('Error connecting to database', err));
   
 
-// POST API endpoint
 app.post('/api/users', async (req, res) => {
   const { name, email, password } = req.body;
 
-  // Validate user data
+  
   if (!name || !email || !password) {
     return res.status(400).json({ message: 'Validation error: All fields are required' });
   }
@@ -31,14 +28,14 @@ app.post('/api/users', async (req, res) => {
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     if (error.name === 'ValidationError') {
-      res.status(400).json(`{ message: Validation error: ${error.message} }`);
+      res.status(400).json({ message: `Validation error: ${error.message}` });
     } else {
       res.status(500).json({ message: 'Server error' });
     }
   }
 });
 
-// Start server
+
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
 });
